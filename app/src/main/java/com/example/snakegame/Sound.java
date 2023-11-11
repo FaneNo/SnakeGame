1f3580063388c65aedd6a2b92db22a0e02bde79d
FaneNo <102693139+FaneNo@users.noreply.github.com>
2023-11-10 20:02:35 -0800

    Separate the sound into another class-phu

---
+++ app/src/main/java/com/example/snakegame/Sound.java
@@ -0,0 +1,59 @@
+package com.example.snakegame;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
+import android.util.Log;
+
+import java.io.IOException;
+
+public class Sound {
+    private SoundPool mSP;
+    private int mEat_ID = -1;
+    private int mCrashID = -1;
+    public Sound(Context context) {
+        // Initialize the SoundPool
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+
+            mSP = new SoundPool.Builder()
+                    .setMaxStreams(5)
+                    .setAudioAttributes(audioAttributes)
+                    .build();
+        } else {
+            mSP = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+        }
+
+        try {
+            AssetManager assetManager = context.getAssets();
+            AssetFileDescriptor descriptor;
+
+            // Prepare the sounds in memory
+            descriptor = assetManager.openFd("get_apple.wav");
+            mEat_ID = mSP.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("snake_death.wav");
+            mCrashID = mSP.load(descriptor, 0);
+
+        } catch (IOException e) {
+            // Handle error
+            Log.d("error", "failed to load sound files");
+        }
+    }
+    public void playEatSound() {
+        Log.d("SoundManager", "Playing eat sound");
+        mSP.play(mEat_ID, 1, 1, 0, 0, 1);
+    }
+
+    public void playCrashSound() {
+        Log.d("SoundManager", "Playing crash sound");
+        mSP.play(mCrashID, 1, 1, 0, 0, 1);
+    }
+}
