841691658139aacb42ed00bd80c51e6959c731a8
kdiep4 <kylediep@csus.edu>
2023-12-11 14:13:44 -0800

    updated notes

--- app/src/main/java/com/example/snakegame/Poison.java
+++ app/src/main/java/com/example/snakegame/Poison.java
@@ -10,26 +10,26 @@ import java.util.Random;
 
 class Poison {
 
-    // The location of the apple on the grid
+    // The location of the poison on the grid
     // Not in pixels
     private Point location = new Point();
 
     // The range of values we can choose from
-    // to spawn an apple
+    // to spawn a bottle of poison
     private Point mSpawnRange;
     private int mSize;
 
-    // An image to represent the apple
+    // An image to represent the poison
     private Bitmap mBitmapPoison;
 
-    /// Set up the green apple in the constructor
+    /// Set up the poison in the constructor
     Poison(Context context, Point sr, int s){
 
         // Make a note of the passed in spawn range
         mSpawnRange = sr;
-        // Make a note of the size of a green apple
+        // Make a note of the size of a poison bottle
         mSize = s;
-        // Hide the apple off-screen until the game starts
+        // Hide the poison off-screen until the game starts
         location.x = -10;
 
         // Load the image to the bitmap
@@ -39,9 +39,9 @@ class Poison {
         mBitmapPoison = Bitmap.createScaledBitmap(mBitmapPoison, s, s, false);
     }
 
-    // This is called every time a green apple is eaten
+    // This is called every time a poison is drank
     void spawn(){
-        // Choose two random values and place the apple
+        // Choose two random values and place the poison
         Random random = new Random();
         location.x = random.nextInt(mSpawnRange.x) + 1;
         location.y = random.nextInt(mSpawnRange.y - 1) + 1;
@@ -51,7 +51,7 @@ class Poison {
         location.x = -10;
     }
 
-    // Let SnakeGame know where the green apple is
+    // Let SnakeGame know where the poison is
     // SnakeGame can share this with the snake
     Point getLocation(){
         return location;
